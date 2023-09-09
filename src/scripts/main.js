6bafd66e00c4924e12b47e4807984b97f6c1a7cf
Will H <will.wkhu@gmail.com>
2023-09-09 00:23:04 -0700

    Comment code, update to reflect name change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.desktopLinksWrapper');
+const navMenu = document.querySelector('.linksWrapper');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  })
-);
+// document.querySelectorAll('.nav-link').forEach((n) =>
+//   n.addEventListener('click', () => {
+//     hamburger.classList.remove('active');
+//     navMenu.classList.remove('active');
+//   })
+// );
